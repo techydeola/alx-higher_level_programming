2c606cc231519c17e4b0fe09f4369b7afaa8ebf1
Johnson Adeola <johnsamxy@gmail.com>
2023-08-08 19:20:17 +0100

    new task

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-letter = "abcdefghijklmnopqrstuvwxyz"
-
-
 def islower(c):
-    if c in letter:
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
         return True
-    else:
-        return False
+    return False
