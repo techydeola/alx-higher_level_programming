1979d9bcb41f92acf258f5328cbe3fb8cf9b6969
Johnson Adeola <johnsamxy@gmail.com>
2023-08-11 14:50:43 +0100

    quick fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
+def print_arg(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d} argument.".format(n))
+        return
+    else:
+        if n == 1:
+            print("{:d} argument:".format(n))
+        else:
+            print("{:d} arguments:".format(n))
+        i = 1
+        while i <= n:
+            print("{:d}: {:s}".format(i, argv[i]))
+            i += 1
 
-import sys
-
-elements = list(sys.argv)
-length = len(elements)
-j = 1
-
-if length - 1 == 0:
-    print("0 arguments.")
-    exit()
-
-print("{} arguments:".format(length - 1))
-for i in elements[1:]:
-    print("{:d}: {:s}".format(j, i))
-    j += 1
+if __name__ == "__main__":
+    import sys
+    print_arg(sys.argv)
