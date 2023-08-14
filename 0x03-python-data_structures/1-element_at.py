6321fc2254bb9752ed7a92126c69fdf697da35c3
Johnson adeola <johnsamxy@gmail.com>
2023-08-14 18:46:46 +0100

    quick fix

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,9 +4,7 @@
 def element_at(my_list, idx):
     length = len(my_list)
 
-    if idx < 0:
-        return None
-    elif idx  >= length:
+    if idx < 0 or idx >= length:
         return None
     else:
         return (my_list[idx])
