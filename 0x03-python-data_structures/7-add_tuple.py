2408b2f6fc502bdc3f512332cd004c82476e839a
Johnson adeola <johnsamxy@gmail.com>
2023-08-14 22:52:19 +0100

    new task

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
+
+    if len_a == 1:
+        tuple_a = tuple_a + (0,)
+    if len_b == 1:
+        tuple_b = tuple_b + (0,)
+    if len_a == 0:
+        tuple_a = (0,0)
+    if len_b == 0:
+        tuple_b = (0,0)
+
+    sum_0 = tuple_a[0] + tuple_b[0]
+    sum_1 = tuple_a[1] + tuple_b[1]
+
+    result = (sum_0, sum_1)
+    return result
