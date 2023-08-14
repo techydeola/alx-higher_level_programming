88983d6553bbc0607938d0d8c7174053081fd401
Johnson adeola <johnsamxy@gmail.com>
2023-08-14 18:31:18 +0100

    fixed pycodestyle

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,11 +2,11 @@
 
 
 def element_at(my_list, idx):
-	length = len(my_list)
-	
-	if idx < 0:
-		return None
-	elif idx > length:
-		return None
-	else:
-		return (my_list[idx])
+    length = len(my_list)
+
+    if idx < 0:
+        return None
+    elif idx > length:
+        return None
+    else:
+        return (my_list[idx])
