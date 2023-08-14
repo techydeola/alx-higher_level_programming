33b806f26b8b3126dd1010fba0c5538fb32c297b
Johnson adeola <johnsamxy@gmail.com>
2023-08-14 23:18:07 +0100

    quick fix

--- 0x03-python-data_structures/9-main.py
+++ 0x03-python-data_structures/9-main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 max_integer = __import__('9-max_integer').max_integer
 
-my_list = [-13, -1, -2, -5, 90]
+my_list = [-1]
 max_value = max_integer(my_list)
 print("Max: {}".format(max_value))
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,8 @@
 #!usr/bin/python3
 
 def max_integer(my_list=[]):
+    if my_list == []:
+        return None
     max_int = my_list[0]
     for num in my_list:
         if num > max_int:
