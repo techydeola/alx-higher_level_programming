e65483cd1de158d2d035cad053d498483c211d98
Johnson adeola <johnsamxy@gmail.com>
2023-08-28 20:10:55 +0100

    fixed pycode error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             elements += 1
-        except:
+        except Exception as e:
             pass
     print()
     return elements
