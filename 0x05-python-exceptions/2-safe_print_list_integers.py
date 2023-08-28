01a061e0c5e48ea2ae5f1d150df301217a01446b
Johnson adeola <johnsamxy@gmail.com>
2023-08-28 20:04:22 +0100

    new task

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 
-def safe_print_list(my_list=[], x=0):
+def safe_print_list_integers(my_list=[], x=0):
     elements = 0
     for i in range(x):
         try:
-            print("{}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[i]), end="")
             elements += 1
-        except:
+        except (ValueError, TypeError):
             pass
     print()
     return elements
