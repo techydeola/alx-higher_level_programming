647a1b2fd2bd464b66007fbc4b96e13a3d27c66c
Johnson adeola <johnsamxy@gmail.com>
2023-09-05 22:10:11 +0100

    new task

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+This function returns a copy of a list
+
+"""
+
+
+def copy_list(lst):
+    """
+    copy_list: a function that returns the copy of a list
+
+    Args:
+        l: list to be copied
+    Return:
+        copy of the list
+
+    """
+
+    m = lst[:]
+    return m
