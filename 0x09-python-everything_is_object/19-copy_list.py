5eb5c83662089f3c1b4ebd24d34d1382902ae56c
Johnson adeola <johnsamxy@gmail.com>
2023-09-05 22:14:44 +0100

    fix

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,21 +1,3 @@
 #!/usr/bin/python3
-
-"""
-This function returns a copy of a list
-
-"""
-
-
 def copy_list(lst):
-    """
-    copy_list: a function that returns the copy of a list
-
-    Args:
-        l: list to be copied
-    Return:
-        copy of the list
-
-    """
-
-    m = lst[:]
-    return m
+    return lst[:]
