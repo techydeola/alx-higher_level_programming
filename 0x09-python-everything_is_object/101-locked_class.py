714de0e5208821176d2fd4c32b1af38d17ddc737
Johnson adeola <johnsamxy@gmail.com>
2023-09-05 22:43:15 +0100

    quick fix

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,4 +4,8 @@
 
 
 class LockedClass:
-    __slots__ = ['first_name']
+    def __setattr__(self, name, value):
+        if name == "first_name":
+            super().__setattr__(name, value)
+        else:
+            raise AttributeError("You can only create 'first_name' attribute.")
