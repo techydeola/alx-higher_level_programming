1e23e2fd22f7bc7056d547266e94e0c4bf95a1aa
Johnson adeola <johnsamxy@gmail.com>
2023-09-11 22:15:46 +0100

    new task

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+""" This module contains one class """
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square:
+    """ This class inherits from Rectangle """
+    def __init__(self, size):
+        Rectangle.integer_validator(self, 'square', size)
+        self.__size = size
+
+    def area(self):
+        """ this method claculates the area of a square """
+        return self.__size * self.__size
