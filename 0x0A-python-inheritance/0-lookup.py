82472848f67c478c22b8b62d623811ed757b55e8
Johnson adeola <johnsamxy@gmail.com>
2023-09-11 19:05:19 +0100

    new task

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+This module contains one function
+"""
+
+
+def lookup(obj):
+    """ a function that returns attributes
+        and methods in an object
+    """
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,3 @@
+## Alx Project
+
+- Python Inheritance
