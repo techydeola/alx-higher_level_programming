96d0edfebf8d5cc381cde540cd1c4af8d27da585
Johnson adeola <johnsamxy@gmail.com>
2023-09-11 20:45:29 +0100

    new task

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+""" This module contains one class """
+
+
+class BaseGeometry:
+    """ BaseGeometry: a class
+    """
+
+    def area(self):
+        """ a function that raises an exception """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ this function validates the parameters"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
