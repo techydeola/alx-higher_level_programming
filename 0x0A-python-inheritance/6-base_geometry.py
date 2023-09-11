a8af3a94624bfa338ec9efb9eae5ef050cbc39e5
Johnson adeola <johnsamxy@gmail.com>
2023-09-11 20:27:04 +0100

    new task

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" This module contains one class """
+
+
+class BaseGeometry:
+    """ BaseGeometry: a class
+    """
+    def area(self):
+        """ a function that raises an exception """
+        raise Exception("area() is not implemented")
