8c0ee359f380177ef9bd8c40679aa71cfbc52015
Johnson adeola <johnsamxy@gmail.com>
2023-09-12 21:52:11 +0100

    new task

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+This modules defines one function
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+        to_json_string - a function that writes a json object
+                         into a file
+
+        Return:
+                JSON representation of a string
+    """
+
+    with open(filename, mode="w", encoding="utf-8") as myFile:
+        myFile.write(json.dumps(my_obj))
