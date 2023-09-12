dbd0e08cb2413e41afbd9d287a37dcc9ad0b717f
Johnson adeola <johnsamxy@gmail.com>
2023-09-12 20:44:54 +0100

    new task

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import json
+
+"""
+This modules defines one function
+"""
+
+
+def to_json_string(my_obj):
+    """
+        write_file - a function
+
+        Return:
+                JSON representation of an object
+    """
+    return json.dumps(my_obj)
