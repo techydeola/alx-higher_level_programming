2004f36e70bff1e5ac6327c35aabdbb79c2dc9f7
Johnson adeola <johnsamxy@gmail.com>
2023-09-12 20:05:12 +0100

    quick fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ This modules defines one function that reads a file
 
 def read_file(filename=""):
     """ this function reads a file and prints it to stdout """
-    with open(filename, encoding='UTF-8') as myFile:
+    with open(filename, mode="r", encoding="utf-8") as myFile:
         print(myFile.read())
