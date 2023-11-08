8a2e4ef28c74dc1a6348ce0f68f0038bfcc2eb4a
Johnson adeola <johnsamxy@gmail.com>
2023-11-08 12:14:16 +0100

    task redo

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,2 +1,3 @@
--- Lists all the tables of my database in my MySQL server
+-- a script that lists all the tables of a database in your MySQL server.
+
 SHOW TABLES;
--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,7 @@
--- creates a table called first_table in my current DB
-CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
+-- a script that creates a tables called first_table in
+-- the current database in MySQL server
+
+DROP TABLE IF EXISTS first_table;
+CREATE TABLE first_table (
+	id INT, name VARCHAR(265)
+);
