5fd9b97359e43608afd0d2cd375353075dddeff6
Johnson adeola <johnsamxy@gmail.com>
2023-11-13 19:09:00 +0100

    code fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,6 +4,8 @@ const argLen = process.argv.length - 1;
 
 if (argLen <= 1) {
   console.log('No argument');
-} else {
+} else if (argLen === 2) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
