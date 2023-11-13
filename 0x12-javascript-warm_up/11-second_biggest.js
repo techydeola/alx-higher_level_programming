0f0fd9b29153fb3e378ff48f26d82470e00fb6fe
Johnson adeola <johnsamxy@gmail.com>
2023-11-13 23:18:37 +0100

    new task

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,24 +1,9 @@
 #!/usr/bin/node
-
-let biggest = 0;
-let secondBiggest = 0;
-const myArray = process.argv;
-const arrayLen = myArray.length;
-
-if (arrayLen === 2) {
+if (process.argv.length <= 3) {
   console.log(0);
-  process.exit(0);
-} else if (arrayLen === 3) {
-  console.log(0);
-  process.exit(0);
+} else {
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
-
-for (let i = 2; i < arrayLen; i++) {
-  const temp = parseInt(myArray[i]);
-  if (temp > biggest) {
-    secondBiggest = biggest;
-    biggest = temp;
-  }
-}
-
-console.log(secondBiggest);
