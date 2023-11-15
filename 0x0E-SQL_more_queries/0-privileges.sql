3b30d1cbd03209c848eb21fe52397cb33d55b81a
Johnson adeola <johnsamxy@gmail.com>
2023-11-15 14:51:51 +0100

    redid task

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- Lists all privileges of the users user_0d_1 and user_0d_2
+-- a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2on a the localhost server
+
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
