e03129a1330c11166445eb3c72cf77cb6ee4c4b1
Johnson adeola <johnsamxy@gmail.com>
2024-01-03 19:19:36 +0100

    new task

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,14 @@
+#!/usr/local/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+const fs = require('fs');
+const filePath = process.argv[3];
+
+request(url, function (error, response, body) {
+  if (error) console.error('error:', error);
+  const content = body;
+  fs.writeFile(filePath, content, 'utf-8', function (err, data) {
+    if (err) throw err;
+  });
+});
