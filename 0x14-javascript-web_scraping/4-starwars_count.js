8272ed19ad8a9cb04c44e859c332d22f64adb2c4
Johnson adeola <johnsamxy@gmail.com>
2024-01-03 19:02:06 +0100

    new task

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/local/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+request(url, function (error, response, body) {
+  if (error) console.error('error:', error);
+  const movieData = JSON.parse(body);
+  const data = movieData.results;
+  let count = 0;
+
+  for (const element of data) {
+    for (const character of element.characters) {
+      if (character === 'https://swapi-api.alx-tools.com/api/people/18/') {
+        count += 1;
+      }
+    }
+  }
+  console.log(count);
+});
