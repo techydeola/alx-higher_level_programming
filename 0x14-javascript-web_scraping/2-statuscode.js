202ee78fd8bb21585225b8d833c0d40cf1e6bb31
Johnson adeola <johnsamxy@gmail.com>
2024-01-03 13:57:33 +0100

    new task

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const url = process.argv[2];
+const request = require('request');
+request(url, function (error, response, body) {
+  if (error) console.error('error:', error);
+  console.log('code', response && response.statusCode);
+});
